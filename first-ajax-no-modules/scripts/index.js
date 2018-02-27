d654e4e5123d615bedc28311bb8543751e6ba5d3
biancavee15 <vasquezbmarie@gmail.com>
2018-02-27 16:36:26 -0500

    finished all functions, everything works properly

--- first-ajax-no-modules/scripts/index.js
+++ first-ajax-no-modules/scripts/index.js
@@ -1,4 +1,6 @@
 'use strict';
+/* global test */
+
 const API_KEY = 'AIzaSyB7f9d4tXJRlEKS_tXLRyCSzMyqTVv3K2s';
 
 const store = {
@@ -23,7 +25,7 @@ const fetchVideos = function(searchTerm, callback) {
   $.getJSON(BASE_URL, query, callback);
   
 };
-fetchVideos('tomato', function(data) {console.log(data)});
+//fetchVideos('tomato', function(data) {console.log(data)});
 
 // TASK:
 // 1. Create a `decorateResponse` function that receives the Youtube API response
@@ -34,45 +36,57 @@ fetchVideos('tomato', function(data) {console.log(data)});
 // TEST IT! Grab an example API response and send it into the function - make sure
 // you get back the object you want.
 const decorateResponse = function(response) {
-    const results = response.items.map((item, index) => {
-      const obj = {
+    const results = response.items.map((item,index) => {
+        return {
         id: item.id.videoId,
         title: item.snippet.title,
         thumbnail: item.snippet.thumbnails.default.url 
-      }
-            
-    }
-    
-  );
-)
+        };    
+    }); 
+    console.log("from decorateResponse", results);
+    addVideosToStore(results);
 };
+//fetchVideos('tomato', decorateResponse);
 
 
 // TASK:
 // 1. Create a `generateVideoItemHtml` function that receives the decorated object
 // 2. Using the object, return an HTML string containing all the expected data
 // TEST IT!
-const generateVideoItemHtml = function(video) {
-
+const generateVideoItemHtml = function(item) {
+  
+  return `
+  <li>
+    <p>ID: ${item.id}</p>
+    <p>Title: ${item.title}</p>
+    <p>Thumbnail: ${item.thumbnail}</p>
+  </li>
+  `
 };
+//console.log(generateVideoItemHtml(test));
 
 // TASK:
 // 1. Create a `addVideosToStore` function that receives an array of decorated video 
 // objects and sets the array as the value held in store.items
 // TEST IT!
 const addVideosToStore = function(videos) {
-
+  store.videos = videos
+ // store.videos.push(videos);
+  
+  render();
 };
-
+  //console.log(addVideosToStore(test));
 // TASK:
 // 1. Create a `render` function
 // 2. Map through `store.videos`, sending each `video` through your `generateVideoItemHtml`
 // 3. Add your array of DOM elements to the appropriate DOM element
 // TEST IT!
 const render = function() {
-
+  const result = store.videos.map(video => generateVideoItemHtml(video));
+  $('.results').html(result);
+ console.log(result);
 };
-
+//cannot call outside render();
 // TASK:
 // 1. Create a `handleFormSubmit` function that adds an event listener to the form
 // 2. The listener should:
@@ -85,11 +99,20 @@ const render = function() {
 //   g) Inside the callback, run the `render` function 
 // TEST IT!
 const handleFormSubmit = function() {
+$('form').submit(event => {
+  event.preventDefault();
+  const searchInput = $('#search-term').val();
+  console.log(searchInput);
+  $('#search-term').val("");
+  fetchVideos(searchInput, decorateResponse);
 
+    
+})
 };
 
 // When DOM is ready:
 $(function () {
   // TASK:
   // 1. Run `handleFormSubmit` to bind the event listener to the DOM
+  handleFormSubmit();
 });
---
+++ first-ajax-no-modules/scripts/test.js
@@ -0,0 +1,171 @@
+const test = {
+    "kind": "youtube#searchListResponse",
+    "etag": "\"_gJQceDMxJ8gP-8T2HLXUoURK8c/xVjqXdTt6o8U00neBEFuktFB1RY\"",
+    "nextPageToken": "CAUQAA",
+    "regionCode": "US",
+    "pageInfo": {
+     "totalResults": 1000000,
+     "resultsPerPage": 5
+    },
+    "items": [
+     {
+      "kind": "youtube#searchResult",
+      "etag": "\"_gJQceDMxJ8gP-8T2HLXUoURK8c/0Vr-X4uQ6YddOj8kryiCquBty-o\"",
+      "id": {
+       "kind": "youtube#video",
+       "videoId": "DN61Xcwc4u0"
+      },
+      "snippet": {
+       "publishedAt": "2017-06-08T02:25:43.000Z",
+       "channelId": "UCptL6_qMImyW_yZwiMjQdpg",
+       "title": "A Tour of Every Tomato Plant in My Garden:  All the Variety Names & Ways I am Growing Them",
+       "description": "So I am often asked... What tomato varieties I am growing? How do I grow them? Which ones are my favorites? This is 100% a tour for people that love tomatoes and want to see what The Rusted...",
+       "thumbnails": {
+        "default": {
+         "url": "https://i.ytimg.com/vi/DN61Xcwc4u0/default.jpg",
+         "width": 120,
+         "height": 90
+        },
+        "medium": {
+         "url": "https://i.ytimg.com/vi/DN61Xcwc4u0/mqdefault.jpg",
+         "width": 320,
+         "height": 180
+        },
+        "high": {
+         "url": "https://i.ytimg.com/vi/DN61Xcwc4u0/hqdefault.jpg",
+         "width": 480,
+         "height": 360
+        }
+       },
+       "channelTitle": "Gary Pilarchik  (The Rusted Garden)",
+       "liveBroadcastContent": "none"
+      }
+     },
+     {
+      "kind": "youtube#searchResult",
+      "etag": "\"_gJQceDMxJ8gP-8T2HLXUoURK8c/XVdXqeyEYG-Xu9khijpbqBXcP1A\"",
+      "id": {
+       "kind": "youtube#channel",
+       "channelId": "UCHroTZTdUNXeZOWypt4Akvw"
+      },
+      "snippet": {
+       "publishedAt": "2013-03-26T23:31:42.000Z",
+       "channelId": "UCHroTZTdUNXeZOWypt4Akvw",
+       "title": "Tomato Gaming",
+       "description": "\"Super great to see Tomato ruining my favorite childhood game in a desperate attempt to stay socially relevant\" - Satisfied Viewer.",
+       "thumbnails": {
+        "default": {
+         "url": "https://yt3.ggpht.com/-4Tt9Sd4G3ZY/AAAAAAAAAAI/AAAAAAAAAAA/RhEc98z4I4M/s88-c-k-no-mo-rj-c0xffffff/photo.jpg"
+        },
+        "medium": {
+         "url": "https://yt3.ggpht.com/-4Tt9Sd4G3ZY/AAAAAAAAAAI/AAAAAAAAAAA/RhEc98z4I4M/s240-c-k-no-mo-rj-c0xffffff/photo.jpg"
+        },
+        "high": {
+         "url": "https://yt3.ggpht.com/-4Tt9Sd4G3ZY/AAAAAAAAAAI/AAAAAAAAAAA/RhEc98z4I4M/s800-c-k-no-mo-rj-c0xffffff/photo.jpg"
+        }
+       },
+       "channelTitle": "Tomato Gaming",
+       "liveBroadcastContent": "upcoming"
+      }
+     },
+     {
+      "kind": "youtube#searchResult",
+      "etag": "\"_gJQceDMxJ8gP-8T2HLXUoURK8c/Q0gnUQ8nh-NCNY1an7uon9sDwLM\"",
+      "id": {
+       "kind": "youtube#video",
+       "videoId": "idw9J0DoLks"
+      },
+      "snippet": {
+       "publishedAt": "2009-05-14T15:20:46.000Z",
+       "channelId": "UCuS43BdlIEVYAv-IBBun_Bg",
+       "title": "allah name in tomato",
+       "description": "I found allah name in tomato while i was making food for my father and my mother my mother was sick and i was cooking it happends to me 3 tims...",
+       "thumbnails": {
+        "default": {
+         "url": "https://i.ytimg.com/vi/idw9J0DoLks/default.jpg",
+         "width": 120,
+         "height": 90
+        },
+        "medium": {
+         "url": "https://i.ytimg.com/vi/idw9J0DoLks/mqdefault.jpg",
+         "width": 320,
+         "height": 180
+        },
+        "high": {
+         "url": "https://i.ytimg.com/vi/idw9J0DoLks/hqdefault.jpg",
+         "width": 480,
+         "height": 360
+        }
+       },
+       "channelTitle": "Samim Jamshid",
+       "liveBroadcastContent": "none"
+      }
+     },
+     {
+      "kind": "youtube#searchResult",
+      "etag": "\"_gJQceDMxJ8gP-8T2HLXUoURK8c/dTdT-EGGW2w3P0Npb2mqv4fh44E\"",
+      "id": {
+       "kind": "youtube#video",
+       "videoId": "NskAr0d3Cn8"
+      },
+      "snippet": {
+       "publishedAt": "2016-02-08T20:00:01.000Z",
+       "channelId": "UCp0hYYBW6IMayGgR-WeoCvQ",
+       "title": "Tomato, Tomahto",
+       "description": "Sometimes little things like everyday phrases can cause a disagreement. Just ask Ellen's writers.",
+       "thumbnails": {
+        "default": {
+         "url": "https://i.ytimg.com/vi/NskAr0d3Cn8/default.jpg",
+         "width": 120,
+         "height": 90
+        },
+        "medium": {
+         "url": "https://i.ytimg.com/vi/NskAr0d3Cn8/mqdefault.jpg",
+         "width": 320,
+         "height": 180
+        },
+        "high": {
+         "url": "https://i.ytimg.com/vi/NskAr0d3Cn8/hqdefault.jpg",
+         "width": 480,
+         "height": 360
+        }
+       },
+       "channelTitle": "TheEllenShow",
+       "liveBroadcastContent": "none"
+      }
+     },
+     {
+      "kind": "youtube#searchResult",
+      "etag": "\"_gJQceDMxJ8gP-8T2HLXUoURK8c/wLRVHOMyEbQk5wWmrtnaHq4Nvaw\"",
+      "id": {
+       "kind": "youtube#video",
+       "videoId": "bGYs4KS_djg"
+      },
+      "snippet": {
+       "publishedAt": "2010-02-25T06:06:29.000Z",
+       "channelId": "UCl4h_3qTOIUnUX8KCICRM3A",
+       "title": "Potato or Tomato? - Jamie Oliver's Food Revolution | Promo Clip | On Air With Ryan Seacrest",
+       "description": "\"SUBSCRIBE: http://full.sc/UBDdWt Watch as kids in an elementary school class in Huntington, West Virginia have trouble identifying fresh fruits and vegetables. Huntington has been called the...",
+       "thumbnails": {
+        "default": {
+         "url": "https://i.ytimg.com/vi/bGYs4KS_djg/default.jpg",
+         "width": 120,
+         "height": 90
+        },
+        "medium": {
+         "url": "https://i.ytimg.com/vi/bGYs4KS_djg/mqdefault.jpg",
+         "width": 320,
+         "height": 180
+        },
+        "high": {
+         "url": "https://i.ytimg.com/vi/bGYs4KS_djg/hqdefault.jpg",
+         "width": 480,
+         "height": 360
+        }
+       },
+       "channelTitle": "On Air With Ryan Seacrest",
+       "liveBroadcastContent": "none"
+      }
+     }
+    ]
+   };
